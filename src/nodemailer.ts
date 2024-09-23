3ba1e00d0b86e56355a2b47929ade419cf9df11f
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2024-09-24 01:35:12 +0600

    changing templates

--- src/nodemailer.ts
+++ src/nodemailer.ts
@@ -3,10 +3,19 @@ import nodemailer from 'nodemailer'
 export const transporter = nodemailer.createTransport({
   service: "Gmail",
   host: "smtp.gmail.com",
-  port: 465,
+  port: 465,//587,
   secure: true,
   auth: {
-    user: process.env.SMTP_SENDER_EMAIL,
-    pass: process.env.SMTP_PASSWORD,
+    user: 'amulettefun@gmail.com',
+    pass: 'vogbxcqqesgvvgrq'
   },
 })
+
+//'info@point-medical.com'
+//,'Ali@234851'
+/*
+process.env.SMTP_SENDER_EMAIL
+process.env.SMTP_PASSWORD
+
+
+*/
--- src/send-mail.ts
+++ src/send-mail.ts
@@ -2,30 +2,74 @@ import { Request, Response } from "express";
 import { transporter } from "./nodemailer";
 
 export const sendMail = async (req: Request, res: Response) => {
-  const { email: receiverEmail } : any = req.body; 
+  const {
+    name: receiverName,
+    email: receiverEmail,
+    subject,
+    message,
+    adminEmail,
+  }: any = req.body; 
   console.log('receiver email: ', receiverEmail)
 
 
-  const mailOptions = {
+  const mailOptionsForAdmin = {
+    from: process.env.SMTP_SENDER_EMAIL,
+    to: adminEmail || 'rahat.cse5.bu@gmail.com',
+    subject: "Point Medical Inquiry",
+    text: `
+    Name: ${receiverName},
+    Email: ${receiverEmail},
+    Subject: ${subject},
+    Message: ${message}
+		`,
+    html: `
+    <div>
+    <p>Name: ${receiverName}</p>
+    <br />
+    <p>Email: ${receiverEmail}</p>
+    <br />
+    <p>Subject: ${subject}</p>
+    <br />
+    <p>Message: ${message}</p>
+    </div>
+    `
+  };
+
+
+  const mailOptionsForUser = {
     from: process.env.SMTP_SENDER_EMAIL,
     to: receiverEmail,
-    subject: "You've received a friend request on Amulette!",
+    subject: "Point Medical || Confirmation: Your inquiry has been submitted",
     text: `
-    Hey User,
+    Dear ${receiverName || 'User'},
+    Thank you for connecting with us.
+    Kindly, be informed that we have received your email, and our team is working to get your inquiry answered.  We will get back to you soon.
 
-    I hope you're doing well!
-    This is testing email.
-
-    Best regards,
-    Sujan Mridha
+    Sincerely,
+    ----
+    Point Medical Team.
 		`,
+    html: `
+    <div>
+    <p>Dear ${receiverName || 'User'},</p>
+    <br />
+    <p>Kindly, be informed that we have received your email, and our team is working to get your inquiry answered.  We will get back to you soon.</p>
+    <br />
+    <p>Sincerely,</p>
+    <br />
+    <p>Point Medical Team.</p>
+    </div>
+    `
   };
+
 
   // console.log("mailOptions: ", mailOptions);
 
   try {
-    const _res = await transporter.sendMail(mailOptions);
-    console.log("Email sent response: ", _res);
+    const _res = await transporter.sendMail(mailOptionsForAdmin);
+    console.log("Admin Email sent response: ", _res?.response);
+    const _res2 = await transporter.sendMail(mailOptionsForUser);
+    console.log("User Email sent response: ", _res2?.response);
     if (_res && _res.messageId) {
       return res.status(201).json({
         success: true,
