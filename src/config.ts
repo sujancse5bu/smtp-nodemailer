2a475597a303b497b28d015ba0aa9bcd508c43fb
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2024-09-26 12:48:32 +0600

    .env vars removed

--- src/config.ts
+++ src/config.ts
@@ -5,12 +5,5 @@ dotenv.config({
 });
 
 export default {
-	port: process.env.PORT,
-	mongo_url:
-		process.env["NODE_ENV"] === "production"
-			? process.env["MONGO_PROD_URL"]
-			: process.env["MONGO_LOCAL_URL"],
-	node_env: process.env.NODE_ENV,
-	bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
-	jwt_secret: process.env.JWT_SECRET,
+	port: '5050',
 };
